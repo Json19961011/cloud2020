a62af4e1e73b89c1c735fbc44325511ec445b1ca
Json19961011 <74534781+Json19961011@users.noreply.github.com>
2021-11-19 21:32:24 +0800

    commit bbbb

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
@@ -26,6 +26,7 @@ public class OrderController {
 
     @PostMapping("/test")
     public MyResult<Payment> create(@RequestBody Payment payment) {
+        System.out.println("bbbbbbb");
         return restTemplate.postForObject(url + "/payment/create", payment, MyResult.class);
     }
 }
